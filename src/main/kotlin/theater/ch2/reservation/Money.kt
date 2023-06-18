be3e18e95c8aacfa1decd5ab7110e18afd07c7f5
Jangwook Ryu <rxjw95@gmail.com>
2023-06-18 15:58:16 +0900

    refactor(정수 표현을 위한 Money times() 변경)

--- src/main/kotlin/theater/ch2/reservation/Money.kt
+++ src/main/kotlin/theater/ch2/reservation/Money.kt
@@ -1,6 +1,8 @@
 package theater.ch2.reservation
 
 import java.math.BigDecimal
+import java.math.MathContext
+import java.math.RoundingMode
 
 data class Money private constructor(
     private val value: BigDecimal
@@ -8,19 +10,23 @@ data class Money private constructor(
 
     fun plus(money: Money): Money = Money(value.plus(money.value))
     fun minus(money: Money): Money {
-        if(value < money.value) {
+        if (value < money.value) {
             throw RuntimeException("가지고 있는 금액보다 큰 금액을 뺄 수 없습니다.")
         }
         return Money(value.minus(money.value))
     }
 
-    fun times(percent: Double): Money = Money(value.times(BigDecimal(percent)))
+    fun times(percent: Double): Money = Money(
+        value
+            .times(BigDecimal(percent))
+            .setScale(0, RoundingMode.HALF_UP)
+    )
 
     fun isLessThan(money: Money) = value < money.value
     fun isGreaterThan(money: Money) = value >= money.value
 
     init {
-        if(value < BigDecimal.ZERO) {
+        if (value < BigDecimal.ZERO) {
             throw RuntimeException("돈은 음수일 수 없습니다.")
         }
     }
